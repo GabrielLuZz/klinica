a3ab5f5afe3dda1199aa5e3f722f6b29ba76790c
Willian <willianx64@gmail.com>
2022-11-01 11:54:44 -0300

    feat: Attendance Model

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,41 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Progress(models.TextChoices):
+    # WAITING = "Waiting Attendance"
+    DEFAULT = "Waiting Attendance"
+    DURING = "During Attendance"
+    FINISHED = "Finished Attendance"
+
+
+class Attendance(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    status = models.CharField(
+        max_length=50,
+        choices=Progress.choices,
+        default=Progress.DEFAULT,
+    )
+    type = models.CharField(max_length=50)
+    information = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    doctor = models.ForeignKey(
+        "doctor.Doctor",
+        on_delete=models.CASCADE,
+        related_name="attendances",
+    )
+    receptionist = models.ForeignKey(
+        "receptionist.Receptionist",
+        on_delete=models.CASCADE,
+        related_name="attendances",
+    )
+    patient = models.ForeignKey(
+        "patient.Patient",
+        on_delete=models.CASCADE,
+        related_name="attendances",
+    )
+
+    def __repr__(self) -> str:
+        return f"<Attendance {self.type} - {self.status}>"
