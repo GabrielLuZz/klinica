e282d02467135963097d4b59403399118968434d
PedroSoprano <vonhishi@gmail.com>
2022-11-01 20:13:44 -0400

    feat: clinic model

--- clinic/models.py
+++ clinic/models.py
@@ -1,6 +1,11 @@
+from email.policy import default
+from pyexpat import model
 from django.db import models
-
+import uuid
 
 # Create your models here.
 class Clinic(models.Model):
-    ...
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    clinic_name = models.CharField(max_length=126)
+    is_ocuped = models.BooleanField(default=False)
+    is_ok = models.BooleanField(default=False)
