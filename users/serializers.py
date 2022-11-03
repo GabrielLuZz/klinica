796643afa6769cd03b8da03e32f0e85f1698aaaa
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-11-03 13:21:19 -0300

    feat: serializer do recepicionista criado

--- users/serializers.py
+++ users/serializers.py
@@ -5,20 +5,65 @@ from addresses.serializers import AddressesSerializer
 
 class PatientSerializer(serializers.ModelSerializer):
 
-    address=AddressesSerializer()
+    address = AddressesSerializer()
 
     class Meta:
-        model=User
-        fields=["id", "username", "cpf", "birth_date", "is_doctor", "is_recepcionist", "address"]
-        read_only_fields = ["id", "is_doctor", "is_recepcionist", "is_superuser"]
+        model = User
+        fields = [
+            "id",
+            "username",
+            "cpf",
+            "birth_date",
+            "is_doctor",
+            "is_recepcionist",
+            "address",
+        ]
+        read_only_fields = [
+            "id",
+            "is_doctor",
+            "is_recepcionist",
+            "is_superuser",
+        ]
         write_only_fields = ["password"]
         unique_fields = ["username", "cpf"]
 
 
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
-        model=User
-        fields=["id","username","password","first_name","last_name","email", "cpf", "birth_date", "is_doctor", "crm", "clinic" ]
+        model = User
+        fields = [
+            "id",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "email",
+            "cpf",
+            "birth_date",
+            "is_doctor",
+            "crm",
+            "clinic",
+        ]
+        read_only_fields = ["id"]
+        write_only_fields = ["password"]
+        unique_fields = ["username", "cpf"]
+
+
+class Recepcionist(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "cpf",
+            "birth_date",
+            "is_doctor",
+            "is_recepcionist",
+        ]
+
         read_only_fields = ["id"]
         write_only_fields = ["password"]
         unique_fields = ["username", "cpf"]
