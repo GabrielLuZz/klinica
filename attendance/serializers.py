347539c5391118c2ec9ce12092777b7ef7a705f7
Willian <willianx64@gmail.com>
2022-11-03 11:14:50 -0300

    feat: attendance create url

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -3,9 +3,19 @@ from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from .models import Attendance
 from users.models import User
+from utils.helpers import get_object_or_404_with_message
+import ipdb
+
 
 
 class AttendanceSerializer(serializers.ModelSerializer):
+    doctor = serializers.SerializerMethodField()
+    receptionist = serializers.SerializerMethodField()
+    patient = serializers.SerializerMethodField()
+    doctor_id = serializers.CharField(write_only= True)
+    receptionist_id = serializers.CharField(write_only= True)
+    patient_id = serializers.CharField(write_only= True)
+
     class Meta:
         model = Attendance
         fields = [
@@ -14,33 +24,66 @@ class AttendanceSerializer(serializers.ModelSerializer):
             "attendance_type",
             "attendance_info",
             "created_at",
+            "doctor_id",
+            "receptionist_id",
+            "patient_id",
             "doctor",
             "receptionist",
             "patient",
         ]
+        # extra_kwargs = {
+        #     "doctor_id": {"write_only": True},
+        #     "receptionist_id": {"write_only": True},
+        #     "patient_id": {"write_only": True},
+        # }
         read_only_fields = ["created_at"]
 
-    doctor = serializers.CharField()
-    receptionist = serializers.CharField()
-    patient = serializers.CharField()
+    def get_doctor(self, obj):
+        attendance = model_to_dict(obj)
+        doctor = [user for user in attendance["users"] if user.is_doctor]
+        return model_to_dict(doctor[0])
+
+    def get_receptionist(self, obj):
+        attendance = model_to_dict(obj)
+        receptionist = [user for user in attendance["users"] if user.is_receptionist]
+        return model_to_dict(receptionist[0])
+
+    def get_patient(self, obj):
+        attendance = model_to_dict(obj)
+        patient = [user for user in attendance["users"] if not user.is_doctor and not user.is_receptionist]
+        return model_to_dict(patient[0])
 
     def create(self, validated_data):
-        doctor_id = validated_data.pop("doctor")
-        receptionist_id = validated_data.pop("receptionist")
-        patient_id = validated_data.pop("patient")
+        doctor_id = validated_data.pop("doctor_id")
+        receptionist_id = validated_data.pop("receptionist_id")
+        patient_id = validated_data.pop("patient_id")
 
-        doctor = get_object_or_404(
-            User, id=doctor_id, is_doctor=True, is_receptionist=False
+        doctor = get_object_or_404_with_message(
+            User,
+            id=doctor_id,
+            is_doctor=True,
+            is_receptionist=False,
+            msg="Doctor not Found",
         )
-        receptionist = get_object_or_404(
-            User, id=receptionist_id, is_doctor=False, is_receptionist=True
+        receptionist = get_object_or_404_with_message(
+            User,
+            id=receptionist_id,
+            is_doctor=False,
+            is_receptionist=True,
+            msg="Receptionist not Found",
         )
-        patient = get_object_or_404(
-            User, id=patient_id, is_doctor=False, is_receptionist=False
+        patient = get_object_or_404_with_message(
+            User,
+            id=patient_id,
+            is_doctor=False,
+            is_receptionist=False,
+            msg="Patient not Found",
         )
 
         attendance_obj = Attendance.objects.create(**validated_data)
         attendance_obj.users.set([doctor, receptionist, patient])
+
+        return attendance_obj
 
 
 class AttendanceDetailSerializer(serializers.ModelSerializer):
@@ -74,4 +117,6 @@ class AttendanceDetailSerializer(serializers.ModelSerializer):
     def get_patient(self, obj):
         attendance = model_to_dict(obj)
         users = attendance.users
-        return {user for user in users if not user.is_doctor and not user.is_receptionist}
+        return {
+            user for user in users if not user.is_doctor and not user.is_receptionist
+        }
--- attendance/views.py
+++ attendance/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import AttendanceSerializer, AttendanceDetailSerializer
+from .models import Attendance
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
+class ListCreateAttendanceView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+    serializer_class = AttendanceSerializer
+    queryset = Attendance.objects
