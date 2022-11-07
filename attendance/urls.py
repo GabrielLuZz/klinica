9dc2817762e3e2c70cfbfa48ca70ba6cbb12528e
unknown <mateusbona28@gmail.com>
2022-11-07 09:36:46 -0300

    fix: fix para rodar aplicação

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,10 +4,6 @@ from . import views
 urlpatterns = [
     path("attendance/", views.CreateAttendanceView.as_view()),
     path("attendance/<attendance_id>/", views.RetrieveUpdateAttendanceView.as_view()),
-<<<<<<< HEAD
-    path("attendance/<user_id>/user/", views.ListAttendancesView.as_view()),
-=======
     path("attendance/<user_id>/user/", views.ListAttendancesOwnerView.as_view()),
     path("attendance/<user_id>/doctor/", views.ListAttendancesDoctorView.as_view()),
->>>>>>> 20e9d5f7335a78af56e2799a41767c1fc7491466
 ]
