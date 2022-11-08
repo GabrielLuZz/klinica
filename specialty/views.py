add6baa374bdc07b0237824c2d322451d4b55d7b
Tales <jobbtales@gmail.com>
2022-11-08 16:20:04 -0300

    fix: adicionado tokens para rota patch e delete

--- specialty/views.py
+++ specialty/views.py
@@ -6,6 +6,9 @@ from rest_framework.generics import (
 from .models import Specialty
 from .serializers import SpecialtySerializer
 
+from rest_framework.permissions import IsAdminUser
+from rest_framework.authentication import TokenAuthentication
+
 
 class ListSpecialtyView(ListAPIView):
     queryset = Specialty.objects.all()
@@ -16,7 +19,13 @@ class CreateSpecialtyView(CreateAPIView):
     queryset = Specialty.objects.all()
     serializer_class = SpecialtySerializer
 
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+
 
 class DetailSpecialtyView(RetrieveUpdateDestroyAPIView):
     queryset = Specialty.objects.all()
     serializer_class = SpecialtySerializer
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
